be3a57f4943714b5d7f95bf06c65bfc60a5d50e6
Arjun Bala <er.arjunbala@gmail.com>
2024-03-11 10:39:17 +0530

    Lecture 11-03-2024

---
+++ DemoException.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class DemoException{
+
+	public static void main(String[] args){
+		parents();
+	}
+
+	public static void parents(){
+		try{
+			trustee();	
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void trustee() throws Exception{
+		GopiMam();
+	}
+
+	public static void GopiMam() throws Exception {
+		Arjun();	
+		
+	}
+
+	public static void Arjun() throws Exception{
+		int balance = 10000;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Amount to withdraw");
+		int temp = sc.nextInt();
+		if(temp>balance){
+			throw new Exception("Insufficient Balance");
+		}
+		else{
+			balance = balance - temp;
+			System.out.println("Current Balance = "+balance);	
+		}
+		
+		
+	}
+}
